04ffdde5068152eb99e9ab18d1437080298929da
jackjohnson <me@email.com>
2020-08-27 21:53:11 +0545

    sum comment

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hhh
+{
+    class First
+    {
+       private static void Main(string[] args)
+        { //  var name="jonson";
+        //     Console.WriteLine("Hello " + name);
+
+        Console.WriteLine("What is your name? ");
+        var name=Console.ReadLine();
+        var date=DateTime.Now;
+        Console.WriteLine($"Hello , {name}  on {date:d} at {date:t}");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey(true);
+
+        }
+    }
+}
